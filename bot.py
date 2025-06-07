a7e369493e4b30ed7e25da74626076e978c57292
Karol Oledzki <karolek.oledzki@gmail.com>
2025-06-08 00:40:44 +0200

    new commands added

--- bot.py
+++ bot.py
@@ -26,5 +26,32 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.lower() == "chuj":
+        await message.channel.send("ci w dupe")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def testgigga(ctx):
+    await ctx.send(f'hello my nigga {ctx.author}')
+
+@bot.command()
+async def calc(ctx, *, expression: str):
+    try:
+        # Pozwalamy tylko na cyfry i podstawowe operatory
+        allowed = "0123456789+-*/(). "
+        if not all(char in allowed for char in expression):
+            await ctx.send("Dozwolone są tylko cyfry i operatory + - * / ( )")
+            return
+        result = eval(expression)
+        await ctx.send(f"Wynik: {result}")
+        
+    except Exception as e:
+        await ctx.send("Błąd w wyrażeniu.")
 
 bot.run(TOKEN)
